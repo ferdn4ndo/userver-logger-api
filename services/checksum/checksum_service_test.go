f4e920d5a4f5492ac8519d10e5311eb938193468
Fernando Constantino <const.fernando@gmail.com>
2022-04-21 17:59:17 -0300

    Fixing failing UT

--- services/checksum/checksum_service_test.go
+++ services/checksum/checksum_service_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestComputeFileChecksum(test *testing.T) {
 	fileFullPath := "/go/src/github.com/ferdn4ndo/userver-logger-api/fixture/sample-app.log"
-	expectedChecksum := "3c165740a1b151917f516e37a8f51cfdd72f01d27fbcd325d81cb75aa3dd47b8"
+	expectedChecksum := "4c2a6ae0d9ac1c01e8f8f0d418830b6486ed4bab806f30d829e2af71348cc61d"
 
 	computedChecksum, _ := ComputeFileChecksum(fileFullPath)
 	if expectedChecksum != computedChecksum {
