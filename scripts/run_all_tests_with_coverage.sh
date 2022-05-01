112095100163fcc9836f3b4ea6679494a743ff9f
Fernando Constantino <const.fernando@gmail.com>
2022-04-30 22:13:33 -0300

    Added race testing

--- scripts/run_all_tests_with_coverage.sh
+++ scripts/run_all_tests_with_coverage.sh
@@ -5,13 +5,12 @@
 cd /go/src/github.com/ferdn4ndo/userver-logger-api/
 
 echo "Running tests (with coverage)..."
-go test -coverprofile="$DATA_FOLDER/$COVERAGE_DATA_FILE" ./... 
+go test -race -covermode=atomic -coverprofile="$DATA_FOLDER/$COVERAGE_DATA_FILE" ./... 
 
 echo "Generating report coverage..."
 go tool cover --func "$DATA_FOLDER/$COVERAGE_DATA_FILE" > "$DATA_FOLDER/$COVERAGE_REPORT_FILE"
 
 echo "\nCOVERAGE REPORT (PER FUNCTION):\n"
 cat "$DATA_FOLDER/$COVERAGE_REPORT_FILE"
-echo ""
 
 echo "Report exported to '$DATA_FOLDER/$COVERAGE_REPORT_FILE'!"
