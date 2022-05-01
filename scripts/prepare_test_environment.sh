a7cafd7f51af99e1f457f08a2253a40055f185a3
Fernando Constantino <const.fernando@gmail.com>
2022-04-30 22:13:05 -0300

    Changed coverage output file path

--- scripts/prepare_test_environment.sh
+++ scripts/prepare_test_environment.sh
@@ -13,7 +13,7 @@ export TEST_DATABASE_FILE=test.sqlite.db
 export EMPTY_DATABASE_FILE=empty.sqlite.db
 
 export COVERAGE_DATA_FILE=coverage.out
-export COVERAGE_REPORT_FILE=coverage.report.txt
+export COVERAGE_REPORT_FILE=coverage.xml
 
 /go/src/github.com/ferdn4ndo/userver-logger-api/scripts/reset_test_database.sh
 
